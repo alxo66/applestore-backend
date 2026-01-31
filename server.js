0b4fda5d7bcc35d2cae8b5bafa5599f3b2d7c422
alxo66 <alxo6677@gmail.com>
2026-01-31 23:17:59 +0500

    server.js

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
+import express from 'express';
 
-const depositRoutes = require('./routes/deposit')
-
-// ⏱ запускаем cron
-require('./cron/checkPayments')
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/deposit', depositRoutes)
+const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('AppleStore backend is running 🚀')
-})
+  res.send('AppleStore backend is running 🚀');
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});

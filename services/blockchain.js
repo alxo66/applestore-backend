c4caf2915a9d4e1232745e4d1fa82dce48e14f9d
alxo66 <alxo6677@gmail.com>
2026-01-31 22:37:01 +0500

    blockchain.js

---
+++ services/blockchain.js
@@ -0,0 +1,10 @@
+const checkPayment = async (address, amount) => {
+  // TODO:
+  // BTC: Blockstream
+  // ETH/USDT: Etherscan
+  // TON: tonapi.io
+
+  return false
+}
+
+module.exports = checkPayment

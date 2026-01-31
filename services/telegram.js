49cabd0f33a26c1bb223ed52ed8198f6a51b6414
alxo66 <alxo6677@gmail.com>
2026-01-31 23:02:45 +0500

    telegram.js

--- services/telegram.js
+++ services/telegram.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
-const BOT_TOKEN = process.env.TG_BOT_TOKEN
-const CHAT_ID = process.env.TG_CHAT_ID
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
+const CHAT_ID = process.env.TELEGRAM_CHAT_ID
 
 async function notifyAdmin(text) {
   if (!BOT_TOKEN || !CHAT_ID) return

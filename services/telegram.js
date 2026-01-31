2000c3064566985c69befcd306b8036a6fd2f190
alxo66 <alxo6677@gmail.com>
2026-01-31 22:56:44 +0500

    telegram.js

--- services/telegram.js
+++ services/telegram.js
@@ -1,12 +1,17 @@
 const axios = require('axios')
 
-const sendTelegram = async (text) => {
-  const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`
+const BOT_TOKEN = process.env.TG_BOT_TOKEN
+const CHAT_ID = process.env.TG_CHAT_ID
+
+async function notifyAdmin(text) {
+  if (!BOT_TOKEN || !CHAT_ID) return
+
+  const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`
 
   await axios.post(url, {
-    chat_id: process.env.TELEGRAM_CHAT_ID,
+    chat_id: CHAT_ID,
     text
   })
 }
 
-module.exports = sendTelegram
+module.exports = { notifyAdmin }
